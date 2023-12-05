4245cf101dae9e197eddebcf8f646f9e72727856
KetrabKox <coox144@gmail.com>
2023-12-06 00:08:09 +0100

    Revert "Again bug with adding and deleting files"
    This reverts commit 2f57dcf0a039ae7f8c3b0e5e93e48da55c547dad.

---
+++ frontend/src/components/NavigationView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div></div>
+</template>
+
+<scr setup lang="ts">
+</scr>
+
+<style>
+</style>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <div class="home"></div>
+    <navigation-view />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
